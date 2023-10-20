c02b721b1258a268ec044bbcdf5dd70c8d6b7fe5
purabj <purab.jadhav@merce.co>
2023-10-20 14:47:15 +0530

    comments & sqlc -> db pkg

---
+++ api/server.go
@@ -0,0 +1,18 @@
+package api
+
+import (
+	db "simple/db/sqlc"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Server struct {
+	store  *db.Store
+	router *gin.Engine
+}
+
+func NewServer(store *db.Store) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
+
+}
